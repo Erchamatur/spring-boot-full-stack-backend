81d2f56f6a498995f7c66ec35d10451ad29ef3d9
Erchamatur <pablopiantino@gmail.com>
2021-06-11 11:49:42 -0300

    clean springboot initializer import

---
+++ employeemanager/src/main/java/ar/com/erchamatur/employeemanager/EmployeemanagerApplication.java
@@ -0,0 +1,13 @@
+package ar.com.erchamatur.employeemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeemanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeemanagerApplication.class, args);
+	}
+
+}
---
+++ employeemanager/src/test/java/ar/com/erchamatur/employeemanager/EmployeemanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.erchamatur.employeemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeemanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
