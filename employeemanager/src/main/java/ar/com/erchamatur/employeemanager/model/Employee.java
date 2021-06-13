7e33da052734b6ac8f078cafa51938a093a0ddcd
Erchamatur <pablopiantino@gmail.com>
2021-06-13 19:09:11 -0300

    modelo completo de empleado -> proxima clase, repositorio

--- employeemanager/src/main/java/ar/com/erchamatur/employeemanager/model/Employee.java
+++ employeemanager/src/main/java/ar/com/erchamatur/employeemanager/model/Employee.java
@@ -1,15 +1,103 @@
 package ar.com.erchamatur.employeemanager.model;
 
+import javax.persistence.*;
 import java.io.Serializable;
+
+import static javax.persistence.GenerationType.IDENTITY;
 
 @Entity
 public class Employee implements Serializable{
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(nullable = false, updatable = false)
     private Long id;
     private String name;
     private String email;
     private String jobTitle;
     private String phone;
     private String imageURI;
+
+    public Employee() {
+    }
+
+    public Employee(String name, String email, String jobTitle, String phone, String imageURI, String employeeCode) {
+        this.name = name;
+        this.email = email;
+        this.jobTitle = jobTitle;
+        this.phone = phone;
+        this.imageURI = imageURI;
+        this.employeeCode = employeeCode;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getImageURI() {
+        return imageURI;
+    }
+
+    public void setImageURI(String imageURI) {
+        this.imageURI = imageURI;
+    }
+
+    public String getEmployeeCode() {
+        return employeeCode;
+    }
+
+    public void setEmployeeCode(String employeeCode) {
+        this.employeeCode = employeeCode;
+    }
+
+    @Column(nullable = false, updatable = false)
     private String employeeCode;
 
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", jobTitle='" + jobTitle + '\'' +
+                ", phone='" + phone + '\'' +
+                ", imageURI='" + imageURI + '\'' +
+                ", employeeCode='" + employeeCode + '\'' +
+                '}';
+    }
 }
